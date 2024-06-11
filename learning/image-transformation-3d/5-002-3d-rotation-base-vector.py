a3e60c23a556a3364e1e9a90a54d699c296d3cca
tianmaoyu <tianmaoyu@link-smart.cn>
2024-06-11 18:02:11 +0800

    深度测试-有问题

---
+++ learning/image-transformation-3d/5-002-3d-rotation-base-vector.py
@@ -0,0 +1,145 @@
+from manim import *
+from manim.mobject.opengl.opengl_cylinder import OpenGLCylinder
+from manim.mobject.opengl.opengl_image_mobject import OpenGLImageMobject
+from manim.mobject.opengl.opengl_point_image_mobject import ImagePixelMobject, NumpyImage
+from manim.mobject.opengl.opengl_shpere import OpenGLSphere
+from manim.mobject.opengl.opengl_something import OpenGLCone, OpenGLLine3D
+from manim.mobject.opengl.opengl_surface import OpenGLTexturedSurface, OpenGLSurface
+import numpy as np
+import sympy as sp
+from manim.typing import Image
+from PIL import Image
+
+
+
+#
+class ThreeDRotationVector001(ThreeDScene):
+    def construct(self):
+        self.set_camera_orientation(phi=70 * DEGREES, theta=35 * DEGREES)
+
+        axes = ThreeDAxes(include_numbers=False,  axis_config={"include_ticks": False}, x_range=[-3, 3, 1], y_range=[-3, 3, 1], z_range=[-3, 3, 1], x_length=6,
+                          y_length=6, z_length=6)
+        axes.add(axes.get_axis_labels())
+        # number_plane = NumberPlane(include_numbers=False, x_range=[-7, 7, 1], y_range=[-7, 7, 1], z_range=[-4, 4, 1],
+        #                            x_length=14, y_length=14, z_length=8)
+        # self.play(Create(number_plane))
+        # self.play(Create(axes))
+        vector_length=2
+        # 创建基向量
+        e1 = Vector([1,0,0], color=RED)
+        # e1.add(e1.coordinate_label())
+        e2 = Vector([0,1,0], color=RED)
+        # e1.add(e2.coordinate_label())
+        e3 = Vector([0,0,1], color=RED)
+        # e1.add(e3.coordinate_label())
+
+        dot1= Dot3D([1,0,0])
+        self.add(dot1)
+
+        # 添加标签
+        e1_label = MathTex("e_1").next_to(e1, RIGHT, buff=SMALL_BUFF).scale(0.5).set_color(RED)
+        e2_label = MathTex("e_2").next_to(e2, UP, buff=SMALL_BUFF).scale(0.5).set_color(RED)
+        e3_label = MathTex("e_3").next_to(e3, OUT, buff=SMALL_BUFF).scale(0.5).set_color(RED)
+
+         # NumberLine().normal_vector
+
+        self.add(axes)
+        self.add(e1, e1_label, e2, e2_label, e3, e3_label)
+        self.wait(2)
+        return
+        axes1= axes.copy()
+        axes1.set_color(RED)
+        self.add(axes1)
+        self.wait()
+        theta=30*DEGREES
+        matrix_y = np.array([
+            [np.cos(theta), -np.sin(theta), 0],
+            [np.sin(theta), np.cos(theta), 0],
+            [0, 0, 1]
+        ])
+        self.play(ApplyMethod(axes1.apply_matrix, matrix_y))
+
+        return
+
+
+        start_rad = 30 * DEGREES
+
+        circle = Circle(radius=2, color=BLUE)
+        start_point = circle.point_at_angle(start_rad)
+        start_dot = Dot(point=start_point, color=RED)
+        start_vector = Vector(start_point,color=RED)
+        start_arc = Arc(radius=1, start_angle=0, angle=start_rad, color=RED)
+        start_arc_label = MathTex(r"\theta",color=RED).scale(0.7).next_to(start_arc, RIGHT, buff=0.15)
+
+        self.add(circle,start_dot, start_vector, start_arc,start_arc_label)
+        self.wait()
+
+        start_lines = axes.get_lines_to_point(start_point)
+        start_lines.set_color(RED)
+        self.play(Create(start_lines))
+
+        start_vector_label = MathTex(r"""\begin{bmatrix}x\\y\\z\end{bmatrix}""", color=RED).scale(0.7).next_to(start_dot, direction=RIGHT)
+        self.play(Create(start_vector_label))
+
+
+        #开始动画
+        end_rad = 90 * DEGREES
+        end_point = circle.point_at_angle(end_rad+start_rad)
+        end_vector = start_vector.copy().set_color(YELLOW)
+        end_dot = start_dot.copy().set_color(YELLOW)
+        end_arc = Arc(radius=0.5, start_angle=start_rad, angle=end_rad, color=YELLOW)
+        end_arc_label = MathTex(r"\alpha", color=YELLOW).scale(0.7).next_to(end_arc, UP, buff=0.15)
+
+        self.play(Rotate(end_vector, angle=end_rad, about_point=circle.get_center()),
+                        Rotate(end_dot, angle=end_rad, about_point=circle.get_center()),
+                        Create(end_arc),
+                        Create(end_arc_label),
+                        run_time=2)
+
+
+
+        end_lines = axes.get_lines_to_point(end_point)
+        end_lines.set_color(YELLOW)
+        self.play(Create(end_lines))
+
+        end_vector_label = MathTex(r"""\begin{bmatrix}x'\\y'\\z'\end{bmatrix}""",color=YELLOW).scale(0.7).next_to(end_point,direction=LEFT)
+        self.play(Create(end_vector_label))
+
+        self.move_camera(theta=0 * DEGREES, phi=0 * DEGREES,run_time=1)
+
+        self.move_camera(phi=55 * DEGREES, theta=35 * DEGREES)
+
+        matrix_y= np.array([
+            [0,0,1],
+            [1,0,0],
+            [0,1,0]
+        ])
+        self.play(ApplyMethod(axes.apply_matrix,matrix_y))
+        self.move_camera(theta=0 * DEGREES, phi=0 * DEGREES)
+        self.move_camera(phi=55 * DEGREES, theta=35 * DEGREES)
+        self.wait()
+
+        matrix_y = np.array([
+            [0, 0, 1],
+            [1, 0, 0],
+            [0, 1, 0]
+        ])
+        self.play(ApplyMethod(axes.apply_matrix, matrix_y))
+        self.move_camera(theta=0 * DEGREES, phi=0 * DEGREES)
+        self.move_camera(phi=55 * DEGREES, theta=35 * DEGREES)
+        self.wait()
+
+        matrix_y = np.array([
+            [0, 0, 1],
+            [1, 0, 0],
+            [0, 1, 0]
+        ])
+        self.play(ApplyMethod(axes.apply_matrix, matrix_y))
+        self.move_camera(theta=0 * DEGREES, phi=0 * DEGREES)
+        self.move_camera(phi=55 * DEGREES, theta=35 * DEGREES)
+        self.wait()
+
+
+with tempconfig({"preview": True, "disable_caching": False, "renderer": "opengl"}):
+    ThreeDRotationVector001().render()
+    exit(1)
