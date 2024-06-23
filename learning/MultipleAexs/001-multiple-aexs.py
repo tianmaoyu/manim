0ca27dddf6aa233e43835bf9e20bd80e2c87b2ee
eric <tianmaoyu@foxmail.com>
2024-06-23 22:38:04 +0800

    修改正确

--- learning/MultipleAexs/001-multiple-aexs.py
+++ learning/MultipleAexs/001-multiple-aexs.py
@@ -159,7 +159,7 @@ class MultipleAexs002(ThreeDScene):
         ]);
 
 
-        animate = group.animate.apply_matrix(about_point=[2, 2, 2], matrix=matrix_init@Ry@Rz)
+        animate = group.animate.apply_matrix(about_point=[2, 2, 2], matrix=matrix_init.T@ Ry@Rz@matrix_init.T)
         self.play(animate)
 
 
@@ -167,7 +167,85 @@ class MultipleAexs002(ThreeDScene):
 
         self.wait(1)
 
+# 相机 一步动画--- 改正确
+class MultipleAexs0022(ThreeDScene):
+    def construct(self):
+        self.set_camera_orientation(phi=70 * DEGREES, theta=35 * DEGREES)
 
+        axes = ThreeDAxes(include_numbers=False, x_range=[-3, 3, 1], y_range=[-3, 3, 1], z_range=[-3, 3, 1], x_length=6,
+                          y_length=6, z_length=6)
+        axes.add(axes.get_axis_labels())
+        number_plane = NumberPlane(include_numbers=False, x_range=[-7, 7, 1], y_range=[-7, 7, 1], z_range=[-4, 4, 1],
+                                   x_length=14, y_length=14, z_length=8)
+        self.play(Create(number_plane))
+        self.play(Create(axes))
+
+        axes1 = ThreeDAxes(include_numbers=False, x_range=[-3, 3, 1], y_range=[-3, 3, 1], z_range=[-3, 3, 1],
+                           x_length=6,
+                           y_length=6, z_length=6)
+
+        labels = axes1.get_axis_labels(x_label=MarkupText("北").scale(0.5), y_label=MarkupText("东").scale(0.5),
+                                       z_label=MarkupText("地").scale(0.5))
+        axes1.add(labels)
+        axes1.set_color(YELLOW)
+
+        def uv_func(u, v):
+            x = 1
+            y = u
+            z = v
+            return [x, y, z]
+
+        planSurface = OpenGLSurface(uv_func=uv_func, u_range=[-0.4, 0.4], v_range=[-0.3, 0.3],
+                                    depth_test=False).set_color(RED).set_opacity(0.5)
+
+        self.play(Create(planSurface))
+        self.play(Create(axes1))
+
+        group = Group()
+        group.add(planSurface, axes1)
+
+        rad = -30 * DEGREES
+        matrix_init = np.array([
+            [0, 1, 0],
+            [1, 0, 0],
+            [0, 0, -1]
+        ])
+        self.play(ApplyMethod(group.apply_matrix, matrix_init))
+
+        self.play(group.animate.shift([2, 2, 2]))
+
+        self.move_camera(zoom=0.75, run_time=2)
+
+        yawRad = -34.3 * DEGREES
+        pitchRad = -47.4 * DEGREES
+        # pitchRad = -45 * DEGREES
+        rollRad = 0.0
+        Rz = np.array([
+            [np.cos(yawRad), -np.sin(yawRad), 0],
+            [np.sin(yawRad), np.cos(yawRad), 0],
+            [0, 0, 1]
+        ]);
+
+        Ry = np.array([
+            [np.cos(pitchRad), 0, np.sin(pitchRad)],
+            [0, 1, 0],
+            [-np.sin(pitchRad), 0, np.cos(pitchRad)]
+        ]);
+
+        Rx = np.array([
+            [1, 0, 0],
+            [0, np.cos(rollRad), -np.sin(rollRad)],
+            [0, np.sin(rollRad), np.cos(rollRad)]
+        ]);
+
+
+        animate = group.animate.apply_matrix(about_point=[2, 2, 2], matrix=matrix_init.T@ Ry@Rz@matrix_init.T)
+        self.play(animate)
+
+
+        self.begin_ambient_camera_rotation(rate=0.2)
+
+        self.wait(1)
 # 相机 北东地 分布动画 --错的
 class MultipleAexs003(ThreeDScene):
     def construct(self):
@@ -251,6 +329,94 @@ class MultipleAexs003(ThreeDScene):
 
         animate = group.animate.apply_matrix(about_point=[2, 2, 2], matrix=matrix_init)
         self.play(animate)
+
+# 相机 北东地 分布动画 --改正确
+class MultipleAexs0032(ThreeDScene):
+    def construct(self):
+        self.set_camera_orientation(phi=70 * DEGREES, theta=35 * DEGREES)
+
+        axes = ThreeDAxes(include_numbers=False, x_range=[-3, 3, 1], y_range=[-3, 3, 1], z_range=[-3, 3, 1], x_length=6,
+                          y_length=6, z_length=6)
+        axes.add(axes.get_axis_labels())
+        number_plane = NumberPlane(include_numbers=False, x_range=[-7, 7, 1], y_range=[-7, 7, 1], z_range=[-4, 4, 1],
+                                   x_length=14, y_length=14, z_length=8)
+        self.play(Create(number_plane))
+        self.play(Create(axes))
+
+        axes1 = ThreeDAxes(include_numbers=False, x_range=[-3, 3, 1], y_range=[-3, 3, 1], z_range=[-3, 3, 1],
+                           x_length=6,
+                           y_length=6, z_length=6)
+        labels= axes1.get_axis_labels(x_label=MarkupText("北 X").scale(0.5), y_label=MarkupText("东 Y").scale(0.5),
+                              z_label=MarkupText("地 -Z").scale(0.5))
+        axes1.add(labels)
+        axes1.set_color(YELLOW)
+
+        def uv_func(u, v):
+            x = 1
+            y = u
+            z = v
+            return [x, y, z]
+
+        planSurface = OpenGLSurface(uv_func=uv_func, u_range=[-0.4, 0.4], v_range=[-0.3, 0.3],
+                                    depth_test=False).set_color(RED).set_opacity(0.5)
+
+        self.play(Create(planSurface))
+        self.play(Create(axes1))
+
+        group = Group()
+        group.add(planSurface, axes1)
+
+
+        matrix_init = np.array([
+            [0, 1, 0],
+            [1, 0, 0],
+            [0, 0, -1]
+        ])
+        self.play(ApplyMethod(group.apply_matrix, matrix_init))
+
+        self.play(group.animate.shift([2, 2, 2]))
+
+        self.move_camera(zoom=0.75, run_time=2)
+
+        yawRad = -34.3 * DEGREES
+        pitchRad = -47.4 * DEGREES
+        # pitchRad = -45 * DEGREES
+        rollRad = 0.0
+        Rz = np.array([
+            [np.cos(yawRad), -np.sin(yawRad), 0],
+            [np.sin(yawRad), np.cos(yawRad), 0],
+            [0, 0, 1]
+        ]);
+
+        Ry = np.array([
+            [np.cos(pitchRad), 0, np.sin(pitchRad)],
+            [0, 1, 0],
+            [-np.sin(pitchRad), 0, np.cos(pitchRad)]
+        ]);
+
+        Rx = np.array([
+            [1, 0, 0],
+            [0, np.cos(rollRad), -np.sin(rollRad)],
+            [0, np.sin(rollRad), np.cos(rollRad)]
+        ]);
+
+
+        animate = group.animate.apply_matrix(about_point=[2, 2, 2], matrix=matrix_init.T)
+        self.play(animate)
+
+
+        animate = group.animate.apply_matrix(about_point=[2, 2, 2], matrix=Rz)
+        self.play(animate)
+
+        animate = group.animate.apply_matrix(about_point=[2, 2, 2], matrix=Ry)
+        self.play(animate)
+
+        # animate = group.animate.apply_matrix(about_point=[2, 2, 2], matrix=Rx)
+        # self.play(animate)
+
+        animate = group.animate.apply_matrix(about_point=[2, 2, 2], matrix=matrix_init.T)
+        self.play(animate)
+
 
 # 相机 北东地  先不旋转- 最后一步转 -分部
 class MultipleAexs004(ThreeDScene):
@@ -410,5 +576,6 @@ class MultipleAexs005(ThreeDScene):
 
 
 with tempconfig({"preview": True, "disable_caching": False, "renderer": "opengl"}):
-    MultipleAexs002().render()
+    MultipleAexs0032().render()
+    # MultipleAexs002().render()
     exit(1)
